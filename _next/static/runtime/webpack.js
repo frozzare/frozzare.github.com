a9a8604017a8f8ee949b80e4601f1165922cede2
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2020-01-08 10:33:53 +0100

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f40591a0dee62db7bbf1";
+/******/ 	var hotCurrentHash = "72bafce38c71b205b271";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
