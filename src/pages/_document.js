19df1e316229edf14138c974c3514f7a22688ca1
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2021-04-20 19:46:25 +0200

    Add preconnect to fonts and display swap

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,13 @@
 import Document, { Head, Main, NextScript } from 'next/document';
 
 export default class AppDocument extends Document {
-  static async getInitialProps (ctx) {
+  static async getInitialProps(ctx) {
     const isProduction = process.env.NODE_ENV === 'production';
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps, isProduction };
   }
 
-  setGoogleAnalytics () {
+  setGoogleAnalytics() {
     return {
       __html: `(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
         function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
@@ -16,39 +16,41 @@ export default class AppDocument extends Document {
         r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
         ga('set', 'anonymizeIP', true);
         ga('create','UA-20991800-2','auto');
-        ga('send','pageview');`
+        ga('send','pageview');`,
     };
   }
 
-  render () {
+  render() {
     const { isProduction } = this.props;
     return (
-      <html lang='en'>
+      <html lang="en">
         <Head>
-          <meta charSet='UTF-8' />
-          <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-          <meta name='viewport' content='width=device-width, initial-scale=1' />
-          <link rel='dns-prefetch' href='//fonts.googleapis.com/' />
-          <link rel='dns-prefetch' href='//www.google-analytics.com' />
+          <meta charSet="UTF-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="dns-prefetch" href="//fonts.googleapis.com/" />
+          <link rel="dns-prefetch" href="//www.google-analytics.com" />
+          <link rel="dns-prefetch" href="//fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
           <link
-            rel='apple-touch-icon'
-            sizes='180x180'
-            href='/apple-touch-icon.png'
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
           />
           <link
-            rel='icon'
-            type='image/png'
-            sizes='32x32'
-            href='/favicon-32x32.png'
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
           />
           <link
-            rel='icon'
-            type='image/png'
-            sizes='16x16'
-            href='/favicon-16x16.png'
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
           />
-          <link rel='manifest' href='/site.webmanifest' />
-          <link rel='shortcut icon' href='/favicon.ico' />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="shortcut icon" href="/favicon.ico" />
         </Head>
         <body>
           <Main />
@@ -59,7 +61,11 @@ export default class AppDocument extends Document {
             </>
           )}
           <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js" />
-          <script dangerouslySetInnerHTML={{__html:`WebFont.load({ google: { families: ['Roboto'] } });`}} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `WebFont.load({ google: { families: ['Roboto&display=swap'] } });`,
+            }}
+          />
         </body>
       </html>
     );
