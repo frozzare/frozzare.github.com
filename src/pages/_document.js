a3b7111814b0de054c7a8328f483097365ff2e99
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2022-02-19 18:48:37 +0100

    Remove google analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,28 +1,6 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-export default class AppDocument extends Document {
-  static async getInitialProps(ctx) {
-    const isProduction = process.env.NODE_ENV === 'production';
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps, isProduction };
-  }
-
-  setGoogleAnalytics() {
-    return {
-      __html: `(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-        e.src='https://www.google-analytics.com/analytics.js';
-        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-        ga('set', 'anonymizeIP', true);
-        ga('create','UA-20991800-2','auto');
-        ga('send','pageview');`,
-    };
-  }
-
-  render() {
-    const { isProduction } = this.props;
-    return (
+const Document = () => (
       <Html lang="en">
         <Head>
           <meta charSet="UTF-8" />
@@ -31,7 +9,7 @@ export default class AppDocument extends Document {
           <link rel="dns-prefetch" href="//fonts.googleapis.com/" />
           <link rel="dns-prefetch" href="//www.google-analytics.com" />
           <link rel="dns-prefetch" href="//fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
@@ -55,11 +33,6 @@ export default class AppDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          {isProduction && (
-            <>
-              <script dangerouslySetInnerHTML={this.setGoogleAnalytics()} />
-            </>
-          )}
           <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js" />
           <script
             dangerouslySetInnerHTML={{
@@ -69,5 +42,5 @@ export default class AppDocument extends Document {
         </body>
       </Html>
     );
-  }
-}
+
+export default Document;
